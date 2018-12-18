31d5e563ef6789e67ae647f5d8dad7df97eb5810
Javier <jmartin@grupoloyal.com>
2018-12-18 11:01:14 +0100

    servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndraCoreModule, LoggerService } from 'src/indra-core';
+import { IndraCoreModule, LoggerService, ERROR_LEVEL } from 'src/indra-core';
 import { ClientesModule } from './clientes/clientes.module';
 import { ProveedoresModule } from './proveedores/proveedores.module';
 import { CommonAppModule } from './common-app/common-app.module';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { CommonAppModule } from './common-app/common-app.module';
   ],
   providers: [
     LoggerService,
+    {provide: ERROR_LEVEL, useValue: environment.ERROR_LEVEL },
   ],
   bootstrap: [AppComponent]
 })
