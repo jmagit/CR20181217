fe5d328b891e431ee3f84a8c109bf81c49647a59
Javier <jmartin@grupoloyal.com>
2018-12-19 11:46:10 +0100

    Calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeEsExtra from '@angular/common/locales/extra/es';
+registerLocaleData(localeEs, 'es', localeEsExtra);
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { IndraCoreModule, LoggerService, ERROR_LEVEL } from 'src/indra-core';
 import { ClientesModule } from './clientes/clientes.module';
 import { ProveedoresModule } from './proveedores/proveedores.module';
 import { CommonAppModule } from './common-app/common-app.module';
 import { environment } from 'src/environments/environment';
+
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DemosComponent } from './demos/demos.component';
 import { NotificationComponent } from './notification/notification.component';
 import { DinamicoComponent } from './dinamico/dinamico.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +27,8 @@ import { DinamicoComponent } from './dinamico/dinamico.component';
     HomeComponent,
     DemosComponent,
     NotificationComponent,
-    DinamicoComponent
+    DinamicoComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule, FormsModule,
