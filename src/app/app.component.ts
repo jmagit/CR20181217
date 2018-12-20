8a2cce8db11873b14a266c0e0bb39619ea5faa11
Javier <jmartin@grupoloyal.com>
2018-12-20 12:09:25 +0100

    seguridad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './common-app/seguridad.services';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(public auth: AuthService) {}
+  public login() {
+    if (this.auth.isAutenticated) {
+      this.auth.logout();
+    } else {
+      this.auth.login('Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiYWRtaW4iLCJleHBpcmVzSW4iOiIxaCIsImlhdCI6MTU0NTMwMzkwMn0.YGYvu0ZAFG6pYglm8sS7jCISQuHm5g9-KtEQFC4058Y', 'admin');
+    }
+  }
 }
---
+++ src/app/common-app/seguridad.services.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CanActivate } from '@angular/router/src/utils/preactivation';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private isAuth = false;
+  private authToken: string = null;
+  private name = '(Anonimo)';
+  constructor() {
+    if (localStorage && localStorage.AuthService) {
+      const rslt = JSON.parse(localStorage.AuthService);
+      this.isAuth = rslt.isAuth;
+      this.authToken = rslt.authToken;
+      this.name = rslt.name;
+    }
+  }
+  get AuthorizationHeader() {
+    return this.authToken;
+  }
+  get isAutenticated() {
+    return this.isAuth;
+  }
+  get Name() {
+    return this.name;
+  }
+  login(authToken: string, name: string) {
+    this.isAuth = true;
+    this.authToken = authToken;
+    this.name = name;
+    if (localStorage) {
+      localStorage.AuthService = JSON.stringify({ isAuth: this.isAuth, authToken, name });
+    }
+  }
+  logout() {
+    this.isAuth = false;
+    this.authToken = '';
+    this.name = '';
+    if (localStorage) {
+      localStorage.removeItem('AuthService');
+    }
+  }
+}
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private auth: AuthService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!req.withCredentials || ! this.auth.isAutenticated) {
+      return next.handle(req);
+    }
+    const authReq = req.clone(
+      { headers: req.headers.set('Authorization', this.auth.AuthorizationHeader) });
+    return next.handle(authReq);
+  }
+}
+
+@Injectable({ providedIn: 'root'})
+export class AuthGuard implements CanActivate {
+  path: ActivatedRouteSnapshot[];
+  route: ActivatedRouteSnapshot;
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    return this.authService.isAutenticated;
+  }
+}
